db6d3b4f62e3371f45f6fcf175f7928617d9e6d3
Eugeneiya <eugeneiyab@gmail.com>
2025-08-19 20:05:23 +0300

    Update test_02_calc.py

--- Lesson_6K/test_02_calc.py
+++ Lesson_6K/test_02_calc.py
@@ -7,21 +7,20 @@ from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(
     service=ChromeService(ChromeDriverManager().install()))
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
 
-text_input = driver.find_element(By.CSS_SELECTOR, "#delay").clear()
-text_input = driver.find_element(By.CSS_SELECTOR, "#delay")
-text_input.send_keys("45")
 
-driver.find_element(By.XPATH, "//*[@id='calculator']/div[2]/span[1]").click()
-driver.find_element(By.XPATH, "//*[@id='calculator']/div[2]/span[4]").click()
-driver.find_element(By.XPATH, "//*[@id='calculator']/div[2]/span[2]").click()
-driver.find_element(By.XPATH, "//*[@id='calculator']/div[2]/span[15]").click()
+def test_calc():
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
 
-WebDriverWait(driver, 45).until(
-   EC.text_to_be_present_in_element((By.XPATH, "//*[@id='calculator']/div[1]/div"), "15"))
-
-res = driver.find_element(By.XPATH, "//*[@id='calculator']/div[1]/div").text
-assert res == "15"
-
-driver.quit()
+    text_input = driver.find_element(By.CSS_SELECTOR, "#delay").clear()
+    text_input = driver.find_element(By.CSS_SELECTOR, "#delay")
+    text_input.send_keys("45")
+    driver.find_element(By.XPATH, "//*[@id='calculator']/div[2]/span[1]").click()
+    driver.find_element(By.XPATH, "//*[@id='calculator']/div[2]/span[4]").click()
+    driver.find_element(By.XPATH, "//*[@id='calculator']/div[2]/span[2]").click()
+    driver.find_element(By.XPATH, "//*[@id='calculator']/div[2]/span[15]").click()
+    WebDriverWait(driver, 45).until(
+        EC.text_to_be_present_in_element((By.XPATH, "//*[@id='calculator']/div[1]/div"), "15"))
+    res = driver.find_element(By.XPATH, "//*[@id='calculator']/div[1]/div").text
+    assert res == "15"
+    driver.quit()
